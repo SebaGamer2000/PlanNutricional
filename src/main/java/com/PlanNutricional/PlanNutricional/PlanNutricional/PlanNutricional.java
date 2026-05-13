42cdcfac17a37758d226de3f25d1f62dcb68c7b4
Seba <seb.santis@duocuc.cl>
2026-05-12 21:46:58 -0400

    Modelo, repositorio y dtos listos

--- src/main/java/com/PlanNutricional/PlanNutricional/PlanNutricional/PlanNutricional.java
+++ src/main/java/com/PlanNutricional/PlanNutricional/PlanNutricional/PlanNutricional.java
@@ -1,4 +1,39 @@
 package com.PlanNutricional.PlanNutricional.PlanNutricional;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Planes Nutricionales")
 public class PlanNutricional {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombrePlan;
+
+    @Column
+    private Integer calorias;
+
+    @Column
+    private Integer proteina;
+
+    @Column
+    private Integer carbohidratos;
+
+    @Column
+    private Integer grasas;
+
+    @Column (nullable = false)
+    private String momento;
+
+    @Column (nullable = false)
+    private String alimentos;
+
 }
