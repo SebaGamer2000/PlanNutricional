3536af1faa36ccadb811b03b320a746c02684078
Seba <seb.santis@duocuc.cl>
2026-05-12 22:26:12 -0400

    Controller y service listos

--- src/main/java/com/PlanNutricional/PlanNutricional/PlanController/PlanController.java
+++ src/main/java/com/PlanNutricional/PlanNutricional/PlanController/PlanController.java
@@ -1,4 +1,50 @@
 package com.PlanNutricional.PlanNutricional.PlanController;
 
+import com.PlanNutricional.PlanNutricional.PlanDTO.PlanRequestDTO;
+import com.PlanNutricional.PlanNutricional.PlanDTO.PlanResponseDTO;
+import com.PlanNutricional.PlanNutricional.PlanService.PlanService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Planes")
+@RequiredArgsConstructor
 public class PlanController {
+    private final PlanService planService;
+
+    @GetMapping
+    public ResponseEntity<List<PlanResponseDTO>> findAll(){
+        return ResponseEntity.ok(planService.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<PlanResponseDTO> findbyId(@PathVariable Long id){
+        return planService.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<PlanResponseDTO> crear(
+            @Valid @RequestBody PlanRequestDTO dto
+            ){
+        return ResponseEntity.status(201).body(planService.guardar(dto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<PlanResponseDTO> actualizar(
+            @PathVariable Long id, @Valid @RequestBody PlanRequestDTO dto
+    ){
+        return planService.actualizar(id,dto).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> eliminar (@PathVariable Long id){
+        if (planService.findById(id).isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        planService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/PlanNutricional/PlanNutricional/PlanService/PlanService.java
+++ src/main/java/com/PlanNutricional/PlanNutricional/PlanService/PlanService.java
@@ -1,4 +1,66 @@
 package com.PlanNutricional.PlanNutricional.PlanService;
 
+import com.PlanNutricional.PlanNutricional.PlanDTO.PlanRequestDTO;
+import com.PlanNutricional.PlanNutricional.PlanDTO.PlanResponseDTO;
+import com.PlanNutricional.PlanNutricional.PlanNutricional.PlanNutricional;
+import com.PlanNutricional.PlanNutricional.PlanRepository.PlanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+@Service
+@RequiredArgsConstructor
+
 public class PlanService {
+    private final PlanRepository planRepository;
+
+    private PlanResponseDTO maptoDTO(PlanNutricional planNutricional){
+        return new PlanResponseDTO(
+                planNutricional.getId(),
+                planNutricional.getNombrePlan(),
+                planNutricional.getCalorias(),
+                planNutricional.getProteina(),
+                planNutricional.getCarbohidratos(),
+                planNutricional.getGrasas(),
+                planNutricional.getMomento(),
+                planNutricional.getAlimentos()
+        );
+    }
+    public List<PlanResponseDTO> findAll(){
+        return planRepository.findAll().stream().map(this::maptoDTO).collect(Collectors.toList());
+    }
+
+    public Optional<PlanResponseDTO> findById(Long id){
+        return planRepository.findById(id).map(this::maptoDTO);
+    }
+
+    public PlanResponseDTO guardar(PlanRequestDTO dto){
+        PlanNutricional planNutricional = new PlanNutricional(
+                null,
+                dto.getNombrePlan(),
+                dto.getCalorias(),
+                dto.getProteina(),
+                dto.getCarbohidratos(),
+                dto.getGrasas(),
+                dto.getMomento(),
+                dto.getAlimentos()
+        );
+        return maptoDTO(planRepository.save(planNutricional));
+    }
+
+    public Optional<PlanResponseDTO> actualizar(Long id, PlanRequestDTO dto){
+        return planRepository.findById(id).map(existente ->{
+            existente.setNombrePlan(dto.getNombrePlan());
+            existente.setCalorias(dto.getCalorias());
+            existente.setProteina(dto.getProteina());
+            existente.setCarbohidratos(dto.getCarbohidratos());
+            existente.setGrasas(dto.getGrasas());
+            existente.setMomento(dto.getMomento());
+            existente.setAlimentos(dto.getAlimentos());
+            return maptoDTO(planRepository.save(existente));
+        });
+    }
+    public void eliminar(Long id){planRepository.deleteById(id);}
 }
